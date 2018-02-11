ffc2f4b93d02ce55720a213a879c4a52ab346ba9
Jeremy M. Reed <reeje76@gmail.com>
2018-02-11 11:56:51 -0500

    Import code from the Login example project.

---
+++ src/multiclipboard/ClipboardInterfaceController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package multiclipboard;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author jeremyr
+ */
+public class ClipboardInterfaceController {
+  
+  @FXML
+  private Text actiontarget;
+  
+  @FXML
+  protected void handleSubmitButtonAction(ActionEvent event) {
+    actiontarget.setText("Sign in button pressed");
+  }
+}
--- src/multiclipboard/MultiClipboard.java
+++ src/multiclipboard/MultiClipboard.java
@@ -19,7 +19,7 @@ public class MultiClipboard extends Application {
   
   @Override
   public void start(Stage stage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+    Parent root = FXMLLoader.load(getClass().getResource("ClipboardInterfaceLayout.fxml"));
     
     Scene scene = new Scene(root);
     
