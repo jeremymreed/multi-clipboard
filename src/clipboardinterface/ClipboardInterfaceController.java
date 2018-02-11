9b2a301c7c8a8fc984c6555f48d6ecf74d37d662
Jeremy M. Reed <reeje76@gmail.com>
2018-02-11 12:02:29 -0500

    Move ClipboardInterface stuff to separate package.

--- src/clipboardinterface/ClipboardInterfaceController.java
+++ src/clipboardinterface/ClipboardInterfaceController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package multiclipboard;
+package clipboardinterface;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
