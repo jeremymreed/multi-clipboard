1624cc4df8170bfa98ced3208d17dfd3a7f574b3
Jeremy M. Reed <reeje76@gmail.com>
2018-03-10 15:33:05 -0500

    Add About and License Alerts.
    Use Java 8 Alerts to implement the About and License Alerts.
    Set up License alert preferred height and width, so the license
    text looks nice for the user.

--- src/main/java/com/jeremyr/multiclipboard/clipboardinterface/ClipboardInterfaceController.java
+++ src/main/java/com/jeremyr/multiclipboard/clipboardinterface/ClipboardInterfaceController.java
@@ -28,7 +28,10 @@ import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.Region;
 import javafx.scene.text.Text;
 
 /**
@@ -74,12 +77,45 @@ public class ClipboardInterfaceController {
 
   @FXML
   protected void handleAboutAction(ActionEvent event) {
-    // TODO: Show a stage with some interesting information.
+    Alert aboutAlert = new Alert(AlertType.INFORMATION);
+    aboutAlert.setTitle("About");
+    aboutAlert.setHeaderText("About this application");
+    aboutAlert.setContentText("This application was created by Jeremy M. Reed\nVersion: 2.3.1\nThis application is licensed under the MIT License");
+
+    aboutAlert.showAndWait();
   }
 
   @FXML
   protected void handleLicenseAction(ActionEvent event) {
-    // TODO: Show a stage with the text of the MIT license.
+    Alert licenseAlert = new Alert(AlertType.INFORMATION);
+    licenseAlert.setTitle("About");
+    licenseAlert.setHeaderText("The MIT License");
+    licenseAlert.setContentText("Copyright © 2018 Jeremy M. Reed\n"
+            + "\n"
+            + "Permission is hereby granted, free of charge, to any person\n"
+            + "obtaining a copy of this software and associated documentation\n"
+            + "files (the “Software”), to deal in the Software without\n"
+            + "restriction, including without limitation the rights to use,\n"
+            + "copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+            + "copies of the Software, and to permit persons to whom the\n"
+            + "Software is furnished to do so, subject to the following\n"
+            + "conditions:\n"
+            + "\n"
+            + "The above copyright notice and this permission notice shall be\n"
+            + "included in all copies or substantial portions of the Software.\n"
+            + "\n"
+            + "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,\n"
+            + "EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES\n"
+            + "OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\n"
+            + "NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT\n"
+            + "HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,\n"
+            + "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n"
+            + "FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR\n"
+            + "OTHER DEALINGS IN THE SOFTWARE.");
+    licenseAlert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+    licenseAlert.getDialogPane().setMinWidth(550);
+
+    licenseAlert.showAndWait();
   }
 
   @FXML
