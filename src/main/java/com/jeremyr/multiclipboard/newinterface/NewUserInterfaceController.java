b0ddf9da8c847395f55be6cd9027f04ed9da1be8
Jeremy M. Reed <reeje76@gmail.com>
2018-04-28 23:46:53 -0400

    Implement menu bar actions.
    Implement Close, About, and License actions.
    Modify version number in About menu dialog.

--- src/main/java/com/jeremyr/multiclipboard/newinterface/NewUserInterfaceController.java
+++ src/main/java/com/jeremyr/multiclipboard/newinterface/NewUserInterfaceController.java
@@ -25,9 +25,12 @@
 package com.jeremyr.multiclipboard.newinterface;
 
 import java.util.concurrent.atomic.AtomicBoolean;
+import javafx.application.Platform;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.Region;
 
 /**
  *
@@ -70,19 +73,68 @@ public class NewUserInterfaceController {
     return this.shouldNukeClipboard;
   }
 
+  /**
+   * This handler is invoked when the user clicks on the "File.Close" menu item.
+   *
+   * @param event The ActionEvent object describing the event.
+   */
   @FXML
   protected void handleCloseAction(ActionEvent event) {
-    throw new UnsupportedOperationException("Not Implemented yet");
+    Platform.exit();
   }
 
+  /**
+   * This handler is invoked when the user clicks on the "Help.About" menu item.
+   *
+   * @param event The ActionEvent object describing the event.
+   */
   @FXML
   protected void handleAboutAction(ActionEvent event) {
-    throw new UnsupportedOperationException("Not Implemented yet");
+    Alert aboutAlert = new Alert(Alert.AlertType.INFORMATION);
+    aboutAlert.setTitle("About");
+    aboutAlert.setHeaderText("About this application");
+    aboutAlert.setContentText("This application was created by Jeremy M. Reed\nVersion: 5.0.0-rc\nThis application is licensed under the MIT License");
+
+    aboutAlert.showAndWait();
   }
 
+  /**
+   * This handler is invoked when the user clicks on the "Help.License" menu
+   * item.
+   *
+   * @param event The ActionEvent object describing the event.
+   */
   @FXML
   protected void handleLicenseAction(ActionEvent event) {
-    throw new UnsupportedOperationException("Not Implemented yet");
+    Alert licenseAlert = new Alert(Alert.AlertType.INFORMATION);
+    licenseAlert.setTitle("About");
+    licenseAlert.setHeaderText("The MIT License");
+    licenseAlert.setContentText("Copyright © 2018 Jeremy M. Reed\n"
+            + "\n"
+            + "Permission is hereby granted, free of charge, to any person\n"
+            + "obtaining a copy of this software and associated documentation\n"
+            + "files (the “Software”), to deal in the Software without\n"
+            + "restriction, including without limitation the rights to use,\n"
+            + "copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+            + "copies of the Software, and to permit persons to whom the\n"
+            + "Software is furnished to do so, subject to the following\n"
+            + "conditions:\n"
+            + "\n"
+            + "The above copyright notice and this permission notice shall be\n"
+            + "included in all copies or substantial portions of the Software.\n"
+            + "\n"
+            + "THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND,\n"
+            + "EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES\n"
+            + "OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND\n"
+            + "NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT\n"
+            + "HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,\n"
+            + "WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING\n"
+            + "FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR\n"
+            + "OTHER DEALINGS IN THE SOFTWARE.");
+    licenseAlert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+    licenseAlert.getDialogPane().setMinWidth(550);
+
+    licenseAlert.showAndWait();
   }
 
   @FXML
